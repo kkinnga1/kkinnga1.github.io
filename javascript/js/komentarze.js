4178cc21954ecf67e4784e8272a467d05ad05d68
Kinga Korycka <kingakor@o2.pl>
2018-03-10 15:51:51 +0100

    java

---
+++ javascript/js/komentarze.js
@@ -0,0 +1,7 @@
+//tak wyglada komentarz liniowy
+/*to jest komentarz blokowy, możę zawierać kilka linijek, 
+trzeba go zamknąć */
+
+
+
+
---
+++ javascript/js/zmianne.js
@@ -0,0 +1,13 @@
+var imie;
+var nazwisko = "Nowacki";
+var wiek = 32;
+console.log(nazwisko);
+
+imie = "Adam"
+
+console.log(imie);
+
+
+//;aczenie zmiennych
+var laczenieTekstu = "Moje imię to:" + imie + " a nazwisko to:" + nazwisko;
+console.log(laczenieTekstu);

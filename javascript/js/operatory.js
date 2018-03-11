4c5a517566ca6eb3c6adaa171d190a07cac0a50a
Kinga Korycka <kingakor@o2.pl>
2018-03-11 14:48:53 +0100

    14:48

---
+++ javascript/js/operatory.js
@@ -0,0 +1,89 @@
+//operatory arytmeytyczne
+var liczba1 = 10;
+var liczba2 = 5;
+
+//dodawanie
+
+var wynik =liczba1 + liczba2;
+
+
+
+//odejmowanie
+wynik = liczba1 - liczba2;
+
+//mnozenie
+
+wynik = liczba1*liczba2;
+
+//dzielenie
+
+wynik = liczba1 / liczba2;
+
+//modulo
+
+wynik = liczba1 % liczba2;
+
+//inkrementacja
+
+liczba2++;
+
+
+console.log( liczba2);
+
+
+//operatory przypisania 
+
+var liczba3 = 200;
+var liczba4 = 100;
+var liczba5 = 15;
+var liczba6 = 40;
+var liczba7 = 7;
+liczba3 += 200; //liczba3 = liczba3+200
+liczba4 -= 50;
+liczba5 *= 2;
+liczba6 /= 2;
+liczba7 %= 2;
+console.log(wynik);
+
+
+
+//operatory porównania
+
+var liczba8 = 42;
+var liczba9 = 10;
+
+if (liczba8 == liczba9) {
+    console.log("Warunek spełniony, czyli true");
+} else {
+    console.log ("Warunek niespełniony, czyli false");
+}
+
+//iloczyn logiczny
+
+var liczba10 = 20;
+var liczba11  = 4;
+var czyWiosna = "tak";
+
+if(liczba10 < liczba11 && czyWiosna == "tak") {
+    console.log ("warunek spelniony");
+    
+} else {console.log ("warunek niespelniony");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

309ed2086cf828afd5db8f7ae3cee41ae0da3b01
Kinga Korycka <kingakor@o2.pl>
2018-03-11 14:01:37 +0100

    14:01

---
+++ javascript/js/math.js
@@ -0,0 +1,9 @@
+//wlasciwosci obiektu math
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+//metody obieku math
+
+console.log(Math.cos(30));
+console.log(Math.random());
--- javascript/js/zmianne.js
+++ javascript/js/zmianne.js
@@ -1,3 +1,27 @@
+//funkcje w obiektach to metody, czyli funkcja=metoda
+//cyfra nie może być pierwsza, słów kluczowych np var undefined none
+//undefined pojawia się gdy stworzyliśmy zmienną a nic do niej nie zostalo przypisane 
+//nazwy zmiennych mają być zrozumiale, wskazywac co zmienna przechowuje
+//funkcja to instrukcje do wykonania, zmienne są do przechowywania danych 
+/* function nazwa funkcji nawiasy i zawarość funkcji 
+w nawiasach mogą byćparametry, więcej niż jeden parametr trzeba oddzielic przecinkami
+trzeba wpisac zwrot-retunrn funkcji, return jest po to żeby funkcja cos oddala
+//nie zawsze konieczny jest return
+//return zwraca wartość, wartosc przpisywana jest do zmiennej i ta zmienna wykorzystywana jest dalej w programie, po to jest return
+//zasięg zmiennych - miejsca w których funkcja jest wodoczna i można z niej korzystać, sązmienne lokalne i globalne
+//sa zmienne lokalne dostepne tylko w funkcji
+//
+
+
+
+
+
+
+
+
+*/
+
+
 var imie;
 var nazwisko = "Nowacki";
 var wiek = 32;
@@ -8,7 +32,7 @@ imie = "Adam"
 console.log(imie);
 
 
-//;aczenie zmiennych
+//laczenie zmiennych
 var laczenieTekstu = "Moje imię to:" + imie + " a nazwisko to:" + nazwisko;
 console.log(laczenieTekstu);
 
